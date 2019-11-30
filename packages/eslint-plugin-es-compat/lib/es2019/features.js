ad7d5afc46dabd8011785f7001b72ab1eb28bfc1
Robat Williams <robatwilliams@gmail.com>
2019-11-30 21:45:19 +0000

    Consistent ordering

--- packages/eslint-plugin-es-compat/lib/es2019/features.js
+++ packages/eslint-plugin-es-compat/lib/es2019/features.js
@@ -4,14 +4,14 @@ const ruleConfigs = require('./ruleConfigs');
 // TODO incomplete
 module.exports = [
   {
-    ruleConfig: ruleConfigs.noOptionalCatchBinding,
-    compatFeatures: [compatData.javascript.statements.try_catch.optional_catch_binding],
-  },
-  {
     ruleConfig: ruleConfigs.noArrayFlatMethods,
     compatFeatures: [
       compatData.javascript.builtins.Array.flat,
       compatData.javascript.builtins.Array.flatMap,
     ],
+  },
+  {
+    ruleConfig: ruleConfigs.noOptionalCatchBinding,
+    compatFeatures: [compatData.javascript.statements.try_catch.optional_catch_binding],
   },
 ];
