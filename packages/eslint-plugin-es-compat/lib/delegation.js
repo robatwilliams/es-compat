6b869f35716d6963ba0f8973122a9e209f84259e
Robat Williams <robatwilliams@gmail.com>
2019-11-30 17:12:56 +0000

    Extract delegation code into own file

---
+++ packages/eslint-plugin-es-compat/lib/delegation.js
@@ -0,0 +1,45 @@
+function createDelegatee(config, rootContext) {
+  const { definition, options } = config;
+
+  const context = {
+    ...rootContext,
+    getSourceCode: () => rootContext.getSourceCode(), // ESLint adds this later
+    options,
+    report,
+  };
+
+  function report(params) {
+    // Discard fixer; we can't declare the rule as fixable because not all delegatees are.
+    // Look up messageId on delegate meta; report() would look it up on root rule meta.
+    const { fix, message, messageId, ...otherParams } = params;
+
+    rootContext.report({
+      ...otherParams,
+      message: messageId ? definition.meta.messages[messageId] : message,
+    });
+  }
+
+  return definition.create(context);
+}
+
+function delegatingVisitor(delegatees) {
+  const delegator = {};
+
+  delegatees.forEach(visitor => {
+    for (const [key] of Object.entries(visitor)) {
+      delegator[key] = (...args) => delegate(key, args);
+    }
+  });
+
+  function delegate(key, args) {
+    delegatees.forEach(visitor => {
+      if (visitor[key]) {
+        visitor[key](...args);
+      }
+    });
+  }
+
+  return delegator;
+}
+
+module.exports = { createDelegatee, delegatingVisitor };
--- packages/eslint-plugin-es-compat/lib/rule.js
+++ packages/eslint-plugin-es-compat/lib/rule.js
@@ -1,4 +1,5 @@
 const delegateeConfigs = require('./delegateeConfigs');
+const { createDelegatee, delegatingVisitor } = require('./delegation');
 
 module.exports = {
   meta: {
@@ -10,47 +11,3 @@ module.exports = {
     return delegatingVisitor(visitors);
   },
 };
-
-function createDelegatee(config, rootContext) {
-  const { definition, options } = config;
-
-  const context = {
-    ...rootContext,
-    getSourceCode: () => rootContext.getSourceCode(), // ESLint adds this later
-    options,
-    report,
-  };
-
-  function report(params) {
-    // Discard fixer; we can't declare the rule as fixable because not all delegatees are.
-    // Look up messageId on delegate meta; report() would look it up on root rule meta.
-    const { fix, message, messageId, ...otherParams } = params;
-
-    rootContext.report({
-      ...otherParams,
-      message: messageId ? definition.meta.messages[messageId] : message,
-    });
-  }
-
-  return definition.create(context);
-}
-
-function delegatingVisitor(delegatees) {
-  const delegator = {};
-
-  delegatees.forEach(visitor => {
-    for (const [key] of Object.entries(visitor)) {
-      delegator[key] = (...args) => delegate(key, args);
-    }
-  });
-
-  function delegate(key, args) {
-    delegatees.forEach(visitor => {
-      if (visitor[key]) {
-        visitor[key](...args);
-      }
-    });
-  }
-
-  return delegator;
-}
