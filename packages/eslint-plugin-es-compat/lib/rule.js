a8ab1d2b1a15065e19d5b16f5a2f668f0e60588d
Robat Williams <robatwilliams@gmail.com>
2019-11-30 17:00:47 +0000

    Extract delegatee rules into own file

--- packages/eslint-plugin-es-compat/lib/rule.js
+++ packages/eslint-plugin-es-compat/lib/rule.js
@@ -1,45 +1,4 @@
-const eslint = require('eslint');
-const esPlugin = require('eslint-plugin-es');
-
-const eslintCoreRules = new eslint.Linter().getRules();
-
-const delegatees = [
-  {
-    definition: eslintCoreRules.get('no-restricted-properties'),
-    options: [{ object: 'Object', property: 'fromEntries', message: '(ES2019)' }],
-  },
-  {
-    definition: eslintCoreRules.get('no-restricted-syntax'),
-    options: [
-      {
-        selector: 'CallExpression[callee.property.name="flat"]',
-        message: "ES2019 method 'Array.prototype.flat' is forbidden",
-      },
-      {
-        selector: 'CallExpression[callee.property.name="flatMap"]',
-        message: "ES2019 method 'Array.prototype.flatMap' is forbidden",
-      },
-      {
-        // It's a read-only accessor, so can safely ignore assignments to namesakes
-        selector:
-          ':not(AssignmentExpression) > MemberExpression[property.name="description"]',
-        message: "ES2019 property 'Symbol.prototype.description' is forbidden",
-      },
-      {
-        selector:
-          'AssignmentExpression > MemberExpression.right[property.name="description"]',
-        message: "ES2019 property 'Symbol.prototype.description' is forbidden",
-      },
-      {
-        selector: 'CallExpression[callee.property.name="trim"]',
-        message: "ES2019 method 'String.prototype.trim' is forbidden",
-      },
-    ],
-  },
-  { definition: esPlugin.rules['no-json-superset'] },
-  { definition: esPlugin.rules['no-optional-catch-binding'] },
-  { definition: esPlugin.rules['no-rest-spread-properties'] },
-];
+const delegatees = require('./delegatees');
 
 module.exports = {
   meta: {
