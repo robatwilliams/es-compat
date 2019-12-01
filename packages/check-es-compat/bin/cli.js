d8d256afd67abdef2a4f9d708141df49dd302acb
Robat Williams <robatwilliams@gmail.com>
2019-12-01 15:28:11 +0000

    Split out execute function for cli

--- packages/check-es-compat/bin/cli.js
+++ packages/check-es-compat/bin/cli.js
@@ -1,25 +1,31 @@
 #!/usr/bin/env node
 const eslint = require('eslint');
 
-const eslintCLI = new eslint.CLIEngine({
-  useEslintrc: false, // ignore any config files
-  plugins: ['es-compat'],
-  parserOptions: {
-    // Latest version, so all features work
-    ecmaVersion: 2020,
-  },
-  rules: {
-    'es-compat/compat': 'error',
-  },
-});
+const { hasErrors } = execute();
 
-const report = eslintCLI.executeOnFiles(['.']);
-
-const formatter = eslintCLI.getFormatter();
-console.log(formatter(report.results));
-
-if (report.errorCount > 0) {
+if (hasErrors) {
   process.exitCode = 1;
 } else {
   console.log('No issues found. Files analysed are compatible with the target runtimes.');
+}
+
+function execute() {
+  const eslintCLI = new eslint.CLIEngine({
+    useEslintrc: false, // ignore any config files
+    plugins: ['es-compat'],
+    parserOptions: {
+      // Latest version, so all features work
+      ecmaVersion: 2020,
+    },
+    rules: {
+      'es-compat/compat': 'error',
+    },
+  });
+
+  const report = eslintCLI.executeOnFiles(['.']);
+
+  const formatter = eslintCLI.getFormatter();
+  console.log(formatter(report.results));
+
+  return { hasErrors: report.errorCount > 0 };
 }
