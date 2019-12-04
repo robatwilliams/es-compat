9836af0ab8e019691983bfb721e55648972f5553
Robat Williams <robatwilliams@gmail.com>
2019-12-04 20:07:21 +0000

    Tests for ES2018, ES2019

---
+++ packages/eslint-plugin-ecmascript-compat/lib/es2018/features.spec.js
@@ -0,0 +1,58 @@
+const { RuleTester } = require('eslint');
+const rule = require('../rule');
+
+process.env.BROWSERSLIST = 'Chrome >= 59';
+
+const ruleTester = new RuleTester({
+  parserOptions: {
+    ecmaVersion: 2018,
+  },
+});
+
+ruleTester.run('compat', rule, {
+  valid: [],
+  invalid: [
+    {
+      code: 'async function* asyncGenerator() {}',
+      errors: [{ message: 'ES2018 async iteration is forbidden.' }],
+    },
+    {
+      code: 'async () => { for await (const bar of bar) { } }',
+      errors: [{ message: 'ES2018 async iteration is forbidden.' }],
+    },
+    {
+      code: 'const foo = { ...bar };',
+      errors: [{ message: 'ES2018 rest/spread properties are forbidden.' }],
+    },
+    {
+      code: 'const { a, ...rest } = foo;',
+      errors: [{ message: 'ES2018 rest/spread properties are forbidden.' }],
+    },
+    {
+      code: 'foo.finally();',
+      errors: [{ message: "ES2018 method 'Promise.prototype.finally' is forbidden" }],
+    },
+    {
+      code: 'Promise.prototype.finally;',
+      errors: [{ message: "ES2018 method 'Promise.prototype.finally' is forbidden" }],
+    },
+    {
+      code: "/(?<=a)b/.test('look-behind assertion');",
+      errors: [{ message: 'ES2018 RegExp lookbehind assertions are forbidden.' }],
+    },
+    {
+      code: "/(?<a>b)c/.test('named capture group');",
+      errors: [{ message: 'ES2018 RegExp named capture groups are forbidden.' }],
+    },
+    {
+      code: "/./s.test('dotAll flag');",
+      errors: [{ message: "ES2018 RegExp 's' flag is forbidden." }],
+    },
+    {
+      code: "/\\p{Script=Hiragana}+/u.test('Unicode property escape');",
+      errors: [
+        { message: 'ES2018 RegExp Unicode property escape sequences are forbidden.' },
+      ],
+    },
+  ],
+});
---
+++ packages/eslint-plugin-ecmascript-compat/lib/es2019/features.spec.js
@@ -0,0 +1,54 @@
+const { RuleTester } = require('eslint');
+const rule = require('../rule');
+
+process.env.BROWSERSLIST = 'Chrome >= 65';
+
+const ruleTester = new RuleTester({
+  parserOptions: {
+    ecmaVersion: 2019,
+  },
+});
+
+ruleTester.run('compat', rule, {
+  valid: [
+    {
+      code: 'const flat = residentialAddress.flat;',
+    },
+    {
+      code: 'residentialAddress.flat = flat;',
+    },
+  ],
+  invalid: [
+    {
+      code: 'foo.flat();',
+      errors: [{ message: "ES2019 method 'Array.prototype.flat' is forbidden" }],
+    },
+    {
+      code: 'Array.prototype.flatMap;',
+      errors: [{ message: "ES2019 method 'Array.prototype.flatMap' is forbidden" }],
+    },
+    {
+      // eslint-disable-next-line no-irregular-whitespace
+      code: `const u2028 = ' ';`, // line separator character \u2028 is in this string
+      errors: [{ message: "ES2019 '\\u2028' in string literals is forbidden." }],
+    },
+    {
+      code: 'Object.fromEntries();',
+      errors: [
+        { message: "'Object.fromEntries' is restricted from being used. (ES2019)" },
+      ],
+    },
+    {
+      code: 'try {} catch {}',
+      errors: [{ message: "ES2019 optional 'catch' binding is forbidden." }],
+    },
+    {
+      code: 'foo.trimLeft();',
+      errors: [{ message: "ES2019 method 'String.prototype.trimLeft' is forbidden" }],
+    },
+    {
+      code: 'String.prototype.trimEnd;',
+      errors: [{ message: "ES2019 method 'String.prototype.trimEnd' is forbidden" }],
+    },
+  ],
+});
