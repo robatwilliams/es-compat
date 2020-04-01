b71ef93f0ae566e0547c31dc2b478e9888005a4c
Brett Zamir <brettz9@yahoo.com>
2020-04-01 09:42:05 +0800

    Add a `recommended` config

--- packages/eslint-plugin-ecmascript-compat/lib/index.js
+++ packages/eslint-plugin-ecmascript-compat/lib/index.js
@@ -1,4 +1,12 @@
 module.exports = {
+  configs: {
+    recommended: {
+      plugins: ['ecmascript-compat'],
+      rules: {
+        'ecmascript-compat/compat': 'error'
+      }
+    }
+  },
   rules: {
     compat: require('./rule'),
   },
