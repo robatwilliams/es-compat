80ddc2afea08e4f9a31a6f92ae1ae1a77a7b8934
Przemysław Sobstel <XPRZS@saxobank.com>
2022-11-12 14:09:41 +0100

    cleanup

--- packages/eslint-plugin-ecmascript-compat/lib/delegation.js
+++ packages/eslint-plugin-ecmascript-compat/lib/delegation.js
@@ -1,6 +1,5 @@
 function createDelegatee(config, rootContext) {
   const { definition, options } = config;
-  const pollyfils = (rootContext.options && rootContext.options[0] && rootContext.options[0].polyfills) || [];
 
   // All methods except report() are added later. Define necessary ones for used rules.
   const contextLaterAddedMethods = {
@@ -16,25 +15,10 @@ function createDelegatee(config, rootContext) {
     report,
   };
 
-  function matchesPollyfil(data, pollyfil) {
-    if (data.message) {
-      return data.message.includes(`'${pollyfil}'`)
-    }
-    if (data.name) {
-      return data.name === pollyfil;
-    }
-    return false;
-  }
-
   function report(params) {
     // Discard fixer; we can't declare the rule as fixable because not all delegatees are.
     // Look up messageId on delegate meta; report() would look it up on root rule meta.
     const { fix, message, messageId, ...otherParams } = params;
-
-    // Skip report if known polyfill exists
-    if (params.data && pollyfils.some(pollyfil => matchesPollyfil(params.data, pollyfil))) {
-      return;
-    }
 
     rootContext.report({
       ...otherParams,
