00353321c13d3245a4d84b81bc4a824961510848
Robat Williams <robatwilliams@gmail.com>
2024-03-07 16:50:15 +0000

    Update comment

--- packages/check-es-compat/bin/cli.mjs
+++ packages/check-es-compat/bin/cli.mjs
@@ -19,7 +19,7 @@ if (args.length === 0) {
 
 async function execute(files) {
   const eslint = new ESLint({
-    // Ignore any config files
+    // Ignore any config; it's for project's own linting rather than this tool.
     useEslintrc: false,
     ignore: false,
     allowInlineConfig: false,
